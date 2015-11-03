b7c3c0104d5b0fd45c09959aa8a51a8664a267e6
Archimedes Trajano <arch@trajano.net>
2015-11-03 16:08:35 -0500

    Added "precise" to the supported platforms
    This is the one used by Travis CI

--- scripts/common.sh
+++ scripts/common.sh
@@ -7,7 +7,7 @@ function check_platform()
 {
     platform=$(lsb_release -c -s)
     case $platform in
-        "wheezy" | "trusty" | "utopic" | "jessie" | "wily" )
+        "wheezy" | "trusty" | "utopic" | "jessie" | "wily" | "precise" )
             return 0
             ;;
         * )
